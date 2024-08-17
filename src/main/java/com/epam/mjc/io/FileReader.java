c5a1792f98dc978d761eb2cc5c7c6f37b4f3ea5f
Avazkhon <avazxonnazirov334@gmail.com>
2024-08-18 03:24:21 +0500

    first

--- src/main/java/com/epam/mjc/io/FileReader.java
+++ src/main/java/com/epam/mjc/io/FileReader.java
@@ -1,11 +1,44 @@
 package com.epam.mjc.io;
 
-import java.io.File;
-
+import java.io.*;
 
 public class FileReader {
 
     public Profile getDataFromFile(File file) {
-        return new Profile();
+        Profile profile = new Profile();
+
+        try (BufferedReader in = new BufferedReader(new java.io.FileReader(file))) {
+
+
+            String line;
+            while ((line = in.readLine()) != null) {
+                String[] data = line.split(": ");
+
+                String key = data[0];
+                String value = data[1];
+
+                switch(key) {
+                    case "Name":
+                        profile.setName(value);
+                        break;
+                    case "Age":
+                        profile.setAge(Integer.parseInt(value));
+                        break;
+                    case "Email":
+                        profile.setEmail(value);
+                        break;
+                    case "Phone":
+                        profile.setPhone(Long.valueOf(value));
+                        break;
+                }
+            }
+
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+
+
+        return profile;
+
     }
 }
